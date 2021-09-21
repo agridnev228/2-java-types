3ee522d8ec545d1070d59fe92c388dd4f0bd07a4
agridnev228 <agridnev228@gmail.com>
2021-09-21 12:11:13 +0500

    task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -5,17 +5,25 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while(x>=10)
+        {
+            int tmp=x%10;
+            x/=10;
+            if(tmp%2!=0)
+                return "FALSE";
+        }
+        if(x%2!=0)
+            return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
