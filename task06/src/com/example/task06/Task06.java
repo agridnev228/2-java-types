8c17a58e22489a980ad9601b12dd858a86a3a4ca
agridnev228 <agridnev228@gmail.com>
2021-09-21 12:20:48 +0500

    task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,24 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        long res=x+y;
+        int i=0;
+        while (res>=10 || res<=-10)
+        {
+            res/=10;
+            i++;
+        }
+        i++;
+        return i;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
