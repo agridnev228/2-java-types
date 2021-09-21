70550be8a4246870fb2ae5259e1cb329a29ece6c
agridnev228 <agridnev228@gmail.com>
2021-09-21 12:52:23 +0500

    task07

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -5,17 +5,25 @@ public class Task07 {
     public static int solution(int n, int m, int k) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int resn=n%k;
+        int resm=m%k;
+        n-=resn;
+        m-=resm;
+        long ln=n;
+        long lm=m;
+        long res=(ln*lm);
+        res=res/(long)(k*k);
+        return (int)res;
 
-        return 0;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+
+        int result = solution(1000000, 1000000, 50);
         System.out.println(result);
-        */
+
     }
 
 }
